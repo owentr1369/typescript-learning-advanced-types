2b8e985455905c8d21eae0d301b200992bfdaac3
TranTam <tamvaa1306@gmail.com>
2022-09-17 16:37:04 +0700

    discriminated unions

--- dist/app.js
+++ dist/app.js
@@ -41,5 +41,16 @@ function useVehicle(vehicle) {
         vehicle.loadCargo(1000);
     }
 }
-useVehicle(v1);
-useVehicle(v2);
+function moveAnimal(animal) {
+    let speed;
+    switch (animal.type) {
+        case "bird":
+            speed = animal.flyingSpeed;
+            break;
+        case "horse":
+            speed = animal.runningSpeed;
+    }
+    console.log("Moving at speed: " + speed);
+}
+moveAnimal({ type: "bird", flyingSpeed: 200 });
+// Moving at speed: 200
